d08ff9657151301e8a7f4ce0c41bead022fafe01
denerbatista <denerbatista@icloud.com>
2023-01-23 23:56:53 -0300

    fix: white screen

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -12,7 +12,6 @@ function Card({ element }) {
 
       <h2>{element.name}</h2>
 
-      <button onClick={handleSwitchModal()}>Detalhes</button>
       {switchModal && <Modal element={element} />}
     </div>
   );
